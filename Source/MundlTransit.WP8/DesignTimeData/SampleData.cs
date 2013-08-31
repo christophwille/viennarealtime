1980b1c51548f7702fb2300b4f01aa0195dd3d94
Christoph Wille <christoph.wille@gmail.com>
2013-08-31 19:39:06 +0200

    Some designtime data

---
+++ Source/MundlTransit.WP8/DesignTimeData/SampleData.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MundlTransit.WP8.Model;
+
+namespace MundlTransit.WP8.DesignTimeData
+{
+    public class SampleData
+    {
+        public ObservableCollection<SampleLineData> Lines { get; set; }
+        public ObservableCollection<SampleHaltestelleData> Stations { get; set; }
+
+        public SampleData()
+        {
+            Lines = new ObservableCollection<SampleLineData>(CreateLineSampleData());
+            Stations = new ObservableCollection<SampleHaltestelleData>(CreateStationsSampleData());
+        }
+
+        private List<SampleLineData> CreateLineSampleData()
+        {
+            var lines = new List<SampleLineData>()
+            {
+                new SampleLineData()
+                {
+                    Bezeichnung = "U1"
+                },
+                new SampleLineData()
+                {
+                    Bezeichnung = "U2"
+                },
+                new SampleLineData()
+                {
+                    Bezeichnung = "U5"
+                }
+            };
+
+            return lines;
+        }
+
+        private List<SampleHaltestelleData> CreateStationsSampleData()
+        {
+            var stations = new List<SampleHaltestelleData>()
+            {
+                new SampleHaltestelleData()
+                {
+                    Bezeichnung = "Siebenhirten",
+                    SecondaryInformation = "U6"
+                },
+                new SampleHaltestelleData()
+                {
+                    Bezeichnung = "Schottenring",
+                    SecondaryInformation = "U2, U4, 1, 31, 1A"
+                },
+                new SampleHaltestelleData()
+                {
+                    Bezeichnung = "Heiligenstadt",
+                    SecondaryInformation = "U4, D, and, some, more, definitely"
+                }
+            };
+
+            return stations;
+        }
+    }
+}
---
+++ Source/MundlTransit.WP8/DesignTimeData/SampleLineData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MundlTransit.WP8.Data.Reference;
+using WienerLinien.Api;
+using WienerLinien.Api.Ogd;
+
+namespace MundlTransit.WP8.DesignTimeData
+{
+    public class SampleLineData
+    {
+        public MonitorLineType LineType { get; set; }
+        public int Id { get; set; }
+        public string Bezeichnung { get; set; }
+        public int Reihenfolge { get; set; }
+        public bool Echtzeit { get; set; }
+    }
+}
