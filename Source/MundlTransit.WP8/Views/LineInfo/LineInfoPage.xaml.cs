7f9c6b2cebfe74ef07fdd12f8d7739319e8497fc
Christoph Wille <christoph.wille@gmail.com>
2013-09-01 18:05:44 +0200

    Test moving AppBar from XAML to codebehind (necessary for localization)

--- Source/MundlTransit.WP8/Views/LineInfo/LineInfoPage.xaml.cs
+++ Source/MundlTransit.WP8/Views/LineInfo/LineInfoPage.xaml.cs
@@ -5,8 +5,10 @@ using System.Net;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+using CM = Caliburn.Micro;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using MundlTransit.WP8.Resources;
 
 namespace MundlTransit.WP8.Views.LineInfo
 {
@@ -15,6 +17,21 @@ namespace MundlTransit.WP8.Views.LineInfo
         public LineInfoPage()
         {
             InitializeComponent();
+            BuildLocalizedApplicationBar();
+        }
+
+        private void BuildLocalizedApplicationBar()
+        {
+            ApplicationBar = new ApplicationBar();
+
+            var appBarButton = new CM.AppBarButton()
+            {
+                IconUri = new Uri("/Assets/sort.png", UriKind.Relative),
+                Text = "direction",
+                Message = "ChangeDirection"
+            };
+
+            ApplicationBar.Buttons.Add(appBarButton);
         }
     }
 }
