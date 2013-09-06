bbfca3df247ea3653332f74c3d4a03c31abd3f4f
Christoph Wille <christoph.wille@gmail.com>
2013-09-06 11:21:46 +0200

    "Pin to start" for favorites (context menu feature)

--- Source/MundlTransit.WP8/ViewModels/FavoritesViewModel.cs
+++ Source/MundlTransit.WP8/ViewModels/FavoritesViewModel.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 using System.Windows;
 using Caliburn.Micro;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
 using MundlTransit.WP8.Common;
 using MundlTransit.WP8.Data.Runtime;
+using MundlTransit.WP8.Resources;
 using MundlTransit.WP8.Services;
 using MundlTransit.WP8.ViewModels.StationInfo;
 
@@ -17,11 +19,13 @@ namespace MundlTransit.WP8.ViewModels
     {
         private readonly INavigationService navigationService;
         private readonly IDataService _dataService;
+        private readonly IUIService _uiService;
 
-        public FavoritesViewModel(INavigationService navigationService, IDataService ds)
+        public FavoritesViewModel(INavigationService navigationService, IDataService ds, IUIService uisvc)
         {
             _dataService = ds;
             this.navigationService = navigationService;
+            _uiService = uisvc;
         }
 
         public async Task LoadFavoritesAsync()
@@ -52,12 +56,46 @@ namespace MundlTransit.WP8.ViewModels
             });
         }
 
+        public void PinToStart(object sender, Favorite item)
+        {
+            FixUpBindingIssue(sender);
+
+            string navigationUrl = String.Format(
+                "/Views/StationInfo/StationInfoPivotPage.xaml?NavigationStationId={0}", item.HaltestellenId);
+
+            if (null != FindTile(navigationUrl))
+            {
+                _uiService.ShowTextToast(AppResources.Favorites_Favorite_AlreadyPinned, item.Bezeichnung);
+                return;
+            }
+
+            ShellTileData tile = new StandardTileData
+            {
+                Title = item.Bezeichnung,
+                BackgroundImage = new Uri("/Assets/Tiles/FlipCycleTileMediumSecondaryTile.png", UriKind.Relative),
+            };
+
+            ShellTile.Create(new Uri(navigationUrl, UriKind.Relative), tile);
+        }
+
+        private ShellTile FindTile(string partOfUri)
+        {
+            ShellTile shellTile = ShellTile.ActiveTiles.FirstOrDefault(
+                tile => tile.NavigationUri.ToString().Contains(partOfUri));
+
+            return shellTile;
+        }
+
         public void Remove(object sender, Favorite item)
+        {
+            FixUpBindingIssue(sender);
+            PerformRemovalAsync(item);
+        }
+
+        private void FixUpBindingIssue(object sender)
         {
             // http://stackoverflow.com/questions/14444583/contextmenu-in-datatemplate-binding-issue
             ((sender as MenuItem).Parent as ContextMenu).ClearValue(FrameworkElement.DataContextProperty);
-
-            PerformRemovalAsync(item);
         }
 
         private async Task PerformRemovalAsync(Favorite item)
