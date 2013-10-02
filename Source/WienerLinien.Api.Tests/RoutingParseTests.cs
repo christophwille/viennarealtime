a0bb87b48be6fed15b0b53a0d7089e1c12ed433d
Christoph Wille <christoph.wille@gmail.com>
2013-10-02 09:31:40 +0200

    Test with local classes - fails on temp class generation

--- Source/WienerLinien.Api.Tests/RoutingParseTests.cs
+++ Source/WienerLinien.Api.Tests/RoutingParseTests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using WienerLinien.Api.Routing;
+using WienerLinien.Api.Routing.Messages;
 
 namespace WienerLinien.Api.Tests
 {
@@ -14,8 +15,12 @@ namespace WienerLinien.Api.Tests
         [Test]
         public void SimpleRoutingParseTest()
         {
-            var schnittstelle = new RoutingSchnittstelle();
-            bool ok = schnittstelle.ParseRoutingRequestResponse(ResponseFiles.Load(ResponseFiles.RoutingFahrtoptionen8));
+            //var schnittstelle = new RoutingSchnittstelle();
+            //bool ok = schnittstelle.ParseRoutingRequestResponse(ResponseFiles.Load(ResponseFiles.RoutingFahrtoptionen8));
+
+            var ok = MessageSerializationHelper.DeserializeFromString<WienerLinien.Api.Routing.Messages2.itdRequestType>(ResponseFiles.Load(ResponseFiles.RoutingFahrtoptionen8));
+            // HResult=-2146233079
+            // Message=Unable to generate a temporary class (result=1).
 
             Assert.That(ok, Is.EqualTo(true));
         }
