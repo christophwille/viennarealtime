50046a0fa4924925579195547d38e05414b7e3f0
Christoph Wille <christoph.wille@gmail.com>
2013-10-07 13:13:13 +0200

    Remove XSD-based approach to routing

--- Source/WienerLinien.Api.Tests/RoutingParseTests.cs
+++ Source/WienerLinien.Api.Tests/RoutingParseTests.cs
@@ -5,27 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using WienerLinien.Api.Routing;
-using WienerLinien.Api.Routing.Messages;
 
 namespace WienerLinien.Api.Tests
 {
     [TestFixture]
     public class RoutingParseTests
     {
-        [Test]
-        [Ignore]
-        public void BuggyXmlRoutingParseTest()
-        {
-            //var schnittstelle = new RoutingSchnittstelle();
-            //bool ok = schnittstelle.ParseRoutingRequestResponse(ResponseFiles.Load(ResponseFiles.RoutingFahrtoptionen8));
-
-            var ok = MessageSerializationHelper.DeserializeFromString<WienerLinien.Api.Routing.Messages2.itdRequestType>(ResponseFiles.Load(ResponseFiles.RoutingFahrtoptionen8));
-            // HResult=-2146233079
-            // Message=Unable to generate a temporary class (result=1).
-
-            Assert.That(ok, Is.EqualTo(true));
-        }
-
         [Test]
         public void SimpleRoutingParseTest()
         {
