18a8c46bd05e499483c576a7eb320271198ff896
Christoph Wille <christoph.wille@gmail.com>
2013-10-15 14:45:12 +0200

    Bug fix: on deactivation, store station id so when going back, the station's departures can be loaded properly again

---
+++ Source/MundlTransit.WP8/StorageHandlers/StationInfoPivotPageViewModelStorage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using MundlTransit.WP8.ViewModels.StationInfo;
+
+namespace MundlTransit.WP8.StorageHandlers
+{
+    public class StationInfoPivotPageViewModelStorage : StorageHandler<StationInfoPivotPageViewModel>
+    {
+        public override void Configure()
+        {
+            Property(vm => vm.NavigationStationId)
+                .InPhoneState()
+                .RestoreAfterViewLoad();
+        }
+    }
+}
